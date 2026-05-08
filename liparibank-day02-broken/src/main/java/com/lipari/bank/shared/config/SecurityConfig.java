7305765c8910fe279115f64bd2735cfa8dcc871f
Antonino Giuliano <antonino.giuliano@liparipeople.com>
2026-05-08 15:14:26 +0200

    finished

---
+++ liparibank-day02-broken/src/main/java/com/lipari/bank/shared/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.lipari.bank.shared.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain secuirtyFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+        return http.build();
+    }
+}
